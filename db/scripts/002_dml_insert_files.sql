c5e6ebf87a5dae3741a2bb46cf94d2d6490dae48
devvk <info@sekretov.net>
2026-05-14 15:58:50 +0200

    Схема БД

---
+++ db/scripts/002_dml_insert_files.sql
@@ -0,0 +1,22 @@
+INSERT INTO files(name, path)
+VALUES
+    (
+        'interstellar.jpg',
+        'files/posters/interstellar.jpg'
+    ),
+    (
+        'john_wick.jpg',
+        'files/posters/john_wick.jpg'
+    ),
+    (
+        'shrek.jpg',
+        'files/posters/shrek.jpg'
+    ),
+    (
+        'inception.jpg',
+        'files/posters/inception.jpg'
+    ),
+    (
+        'harry_potter.jpg',
+        'files/posters/harry_potter.jpg'
+    );
